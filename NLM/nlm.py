e7442b5aa7003ad04f960eafa482c75c6470d121
kibo35 <kibo35@gmail.com>
2016-03-12 14:02:21 +0900

    debug
    debug

--- NLM/nlm.py
+++ NLM/nlm.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import cv2
 
 parser = argparse.ArgumentParser(description='Non-local means')
+parser.add_argument('input', help='input filename')
 parser.add_argument('--smoothing_strength', '-s', default=1, type=float,
                     help='smoothing strength a.k.a parameter h of non-local means.')
 parser.add_argument('--search_window_size', '-w', default=21, type=int,
@@ -17,20 +18,21 @@ args = parser.parse_args()
 
 r   = 64
 
+input   = args.input
 param_h = args.smoothing_strength
 search_size = args.search_window_size
 patch_size  = args.patch_size
 print   'param_h', param_h, 'search', search_size, 'patch', patch_size
 
-img = cv2.imread('P3052081.JPG')
+img = cv2.imread(input)
 h, w    = img.shape[:2]
 img = cv2.resize(img, (w / 4, h / 4))
 h, w    = img.shape[:2]
-cv2.imwrite('resize.png', img)
+cv2.imwrite('result/resize.png', img)
 
 print   'NLM...'
-dst = cv2.fastNlMeansDenoisingColored(img, param_h, param_h, search_size, patch_size)
-cv2.imwrite('result.png', dst)
+dst = cv2.fastNlMeansDenoisingColored(img, param_h, param_h, patch_size, search_size)
+cv2.imwrite('result/result.png', dst)
 
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 dst = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
@@ -57,6 +59,6 @@ plt.imshow(dst[h/2 - r:h/2 + r, w/2 - r: w/2 + r])
 plt.axis('off')
 
 plt.subplots_adjust(0, 0, 1, 0.9, 0, 0.1)
-plt.savefig('fig.png')
+plt.savefig('result/fig.png')
 
 plt.show()
